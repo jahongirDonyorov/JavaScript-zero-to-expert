307ef63698640f5da3d66ea9b20d6041860f751f
Jahon <donyorovjahon@gmail.com>
2023-03-23 11:19:07 +0000

    ForLoop

--- dars-4/index.js
+++ dars-4/index.js
@@ -38,6 +38,8 @@ console.log('6')
  for(i = 1; i <= 6; i = i + 1) {  // i++ qilsakham ham buladi bu faqat bittadan quwadi hozirgi holatda esa 2,3,4 tadan ham quwiwimiz mumkun
   console.log(i)// local uzgaruvchi i ni chiqarib beradi
   console.log('salom')// salom suziham 6 marotaba takrorlanadi
+  // i++ 3. ni buyirgaham yozib quysak buladi
+  // 4. {} ni ichidagi code tuliq yurib bulgandan sung 3. i++ ishlaydi
  }
 // 1. local uzgaruvchisini yaratib unga qiymat beryapdi
 // 2. usha qiymatni solishtiryapdi
@@ -49,3 +51,48 @@ console.log('6')
 // qiymat quwiw uzgarishi qanday buladi 1 dan 2 gacha ishlaydi 4 ni {} chiqaradi kiyin 1,2,3,4 ishlaydi shu tarzda 6 gacha aylanadi#
 
 // - qiymat berib ayirib ketsak ham buladi
+
+// let vs var 
+//   1
+// for(var i = 1; i <= 6; i++){// 1 local uzgaruzchi 
+
+// }
+// console.log(i)// var flobal bulganligi uchun buyirda chaqirolamiz
+// // letni chaqirsak hato bulardi let local bulsa faqat local ishlaydi var esa hamma joyda ishlayveradi
+ 
+//=====!!!!!!! Continue usha sonni chiqarmasdan utkazib yuboradi
+//=====!!!!!!! Break usha songacha chiqarib beradi faqat
+
+// Break - kirakli songacha chiqaradi qolgani aylanib yotaveradi
+// var sum = 0;
+// for (i = 1; i <= 12; i++){
+//   // console.log(sum += i) // 1,3,6 bub ketadi becaus sum = sum + i = 1 + 1 = sum = 2; 2 + 1 = sum = 3; i ikkita quwdi i qiymati 2 ; 3 + 3 = sum 5; ketaveradi shunday
+//   console.log(`Sum = ${sum} + ${i} = ${(sum += i)}`) // qanday 1,3,5 bubb ketishini kursak buladi consolda
+//   console.log(i)// bu i ni uzi 1,2,3,4 bub ketaveradi
+//   if(i === 10)break;// 9 gacha chiqaradi qolganini chiqarmaydi
+// }
+// // =========
+// //    1       2      3
+// for(i = 1; i < 12; i++){
+//   console.log(i);
+//   i += i; // bu i + i ishlab kiyin 3 ishlaydi +1 = 1+1=2+1=3; 3+3=6+1=7; shu yerda tuxtaydi becaus qolgani 12 dan katta bulib ketadi
+//   // 3. i++ bulmasa uziga uzini quwib ketaveradi 1+1=2; 2+2=4;...
+// }
+
+//============ Toq Juft sonlarni chiqarish
+for(i = 1; i <= 14; i++){
+  // console.log(i % 2)// 1,0,1,0 chiqaveradi juft sonlar bulinib 0 buladi toqlar 1
+  if(i%2 === 0)console.log(`Jusft ${i}`)// i%2 = 0 bulgan sonlarni chiqarsin
+  else console.log(`Toq ${i}`) // i%2=0 bulmagan sonlarni chiqaradi Toq sonlarni
+
+  // 3 ga 0 (qoldiqsiz) bulinadigan solarni olish
+  if( i % 3 === 0) continue;//continue usha sonlarni chiqarmasdan qolganini chiqarib betadi
+
+}
+
+// 3 ga 0 (qoldiqsiz) bulinadigan solarni olish
+for(i = 1; i <= 14; i++){
+  if( i % 3 === 0) continue;//continue usha sonlarni chiqarmasdan qolganini chiqarib betadi
+  if( i % 3 === 0) console.log(i);// bunda 3 ga bulinadiganlarni faqat chiqaradi
+ console.log(i)
+}
