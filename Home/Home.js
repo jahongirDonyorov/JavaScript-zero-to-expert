0ceedee3d865982ab556143b78fd7b9b0a009584
Jahon <donyorovjahon@gmail.com>
2023-03-24 10:56:24 +0000

    HomeWorks

---
+++ Home/Home.js
@@ -0,0 +1,20 @@
+// Toq va Fusf sonlar
+// Fuft
+// for(i = 1; i <= 10; i++){
+//   // console.log(i%2)
+//   if(i%2 === 0)console.log(i)
+//   // else(i)// qilib quysak i%2 = 0 bulmaganlarni (toqlarni) ciqaradi
+// }
+// // Toq
+// for(i = 1; i <= 10; i++){
+//   // console.log(i%2)
+//   if(i%2 === 1)console.log(i)
+// }
+
+// =========== Karra Jadvali
+// for(i = 2; i <= 10; i++){
+//   console.log(`\n${i} - Lik Karra Jadvali`)
+//   for(a = 1; a <= 10; a++){
+//     console.log(`${i} x ${a} = ${i * a}`)
+//   }
+// }
