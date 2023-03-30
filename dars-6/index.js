5fa42291403db300a8b5518d4f0237908e9267c8
Jahon <donyorovjahon@gmail.com>
2023-03-30 18:13:52 +0100

    Object

---
+++ dars-6/index.js
@@ -0,0 +1,15 @@
+// NaNPrimative
+// ================= Object
+// Object {} ichiga yoziladi
+
+// object uzining ichida 1 dan oshiq malumotni jamlay oladi
+
+// let accaunt = 'webbrain' // bu holatda accaunt faqat bitta malumotni uz ichiga ololadi 3, 4, 5 talab malumot quyolmaymiz
+// console.log(accaunt)
+
+// objetkda esa hohlagancha malumot quysam kuladi
+// {1 : 2, name:"Jahon"} - : dan oldin turgan 1 key hisoblanadi, : dan kiyin turgan 2 qiymat, keyni chaqirib qiymatini olsak buladi, , guldan kiyin yana kiy va value bulib ketaveradi
+
+let accaunt = { name: "Jahon", age: 18, day: 14, city: "Juma" }; // bitta verabelga {object} yordamida kuplab malumot beryapmz
+console.log(accaunt);
+ 
